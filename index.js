aa526f87e2af41eb46bfef05e1b280cd4ef1b1ca
Arraying <paul.huebner@googlemail.com>
2019-10-18 16:21:38 +0200

    Testing webhook

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 
 app.get('/', (request, response) => response.send({
     hello: "world",
-    testing: "this"
+    testing: "this",
+    great: "app!"
 }));
 
 app.listen(3000);
